45ca1d1d69f8ce213d71cc0c47551cef14d50ff7
Nahid <n.abdulla1@hotmail.ca>
2022-09-24 15:44:38 +0000

    Completed assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+/*
+1. if actual === expected {
+  console.log(Assertion Passed:[actual] === [expected]);
+} 2. Else {
+  console.log(Assertion Failed: [actual !== [expected]);
+}
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('Blue', 'Gold');
+assertEqual('Red', 'Red');
+assertEqual(765, 567);
+assertEqual(6578, 6578);
+assertEqual(true, false);
