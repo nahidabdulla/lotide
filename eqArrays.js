563ec70cc150fb5a0ddc8da59fab4e326b27e6fd
Nahid <n.abdulla1@hotmail.ca>
2022-09-26 02:40:57 +0000

    created function eqArrays to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Implement a function eqArrays which takes in two arrays and returns 
+// true or false, based on a perfect match.
+
+// Use assertEqual to write test cases for various scenarios.
+
+// It's okay for eqArrays to not return true for nested arrays or arrays 
+// of objects that are identical. We will save this "deeper" problem for 
+// another day.
+
+const eqArrays = function(arr1, arr2) {
+  return arr1.toString() === arr2.toString();
+
+};
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => PASS
+assertEqual(eqArrays([4535, 4355435, 3656464],[342432, 32545, 46543654]), false); // => PASS
+assertEqual(eqArrays(['clouds', 'sun', 'birds'], ['moon', 'stars', 'nightsky']), true); // => FAIL
+assertEqual(eqArrays(['blue', 'red', 'green', 'orange'], ['blue', 'red', 'green', 'orange']), false); // => FAIL
