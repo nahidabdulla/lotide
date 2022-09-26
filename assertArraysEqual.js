0991385cfc6b1d6dd5e78e698fee81b6b895088b
Nahid <n.abdulla1@hotmail.ca>
2022-09-26 21:46:29 +0000

    improved eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,15 @@
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
 
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+  }
+  return true;
 };
 
 
--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,27 @@ const assertEqual = function(actual, expected) {
 };
 
 
-// Implement a function eqArrays which takes in two arrays and returns 
+// Implement a function eqArrays which takes in two arrays and returns
 // true or false, based on a perfect match.
 
 // Use assertEqual to write test cases for various scenarios.
 
-// It's okay for eqArrays to not return true for nested arrays or arrays 
-// of objects that are identical. We will save this "deeper" problem for 
+// It's okay for eqArrays to not return true for nested arrays or arrays
+// of objects that are identical. We will save this "deeper" problem for
 // another day.
 
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
 
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+  }
+  return true;
 };
 
 // Test cases
