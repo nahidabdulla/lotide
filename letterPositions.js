2ed3de7bc31850589bcb77b05d6a694fb9de84c2
Nahid <n.abdulla1@hotmail.ca>
2022-10-03 18:48:58 +0000

    created function letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+const letterPositions = function (sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++){
+    if (sentence[i] === " ") {
+      continue;
+    }
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+}
+
+console.log(letterPositions("hello"));
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
